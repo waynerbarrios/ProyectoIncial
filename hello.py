0c91d732ef084b025161bd0e3b801ba17654a1d0
Wayner Barrios <wbarrios@uninorte.edu.co>
2021-09-18 09:58:16 -0500

    Ultima verison

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello World - linea 1")
 print("Hello World - linea 2")
-print("Hello World - linea 3")
+print("Hello World - linea 3 y 3.5")
+print("Hello World - linea 4")
---
+++ sumatoria.py
@@ -0,0 +1,8 @@
+suma= 0
+suma2= 1
+for i in range(10):
+    suma += i
+    suma2 = suma2*(i+1)
+print("La sumatoria es ", suma)
+print("La productoria es ", suma2)
+print("Nueva linea en sumatoria")
